6ab6ad52c1d7551348f2a559aafbcfc5ec52e1e7
carlos ibarra <caribarra@gmial.com>
2026-04-14 14:59:30 -0500

    RelacionModelos

--- app/Models/Computer.php
+++ app/Models/Computer.php
@@ -5,7 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Courses extends Model
+class Computer extends Model
 {
     use HasFactory;
+    public function apprentice()
+    {
+        return $this->hasOne(Apprentice::class);
+    }
 }
